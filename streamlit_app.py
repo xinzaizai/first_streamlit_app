d859bb862976aa314cff6836dd0ac9d2b824db1e
xinzaizai <77595940+xinzaizai@users.noreply.github.com>
2023-08-23 16:38:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,5 +39,5 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-import connector
+import python
 
